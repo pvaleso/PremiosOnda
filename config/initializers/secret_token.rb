64c281434763924c8ca17dfad1bd09809ae91d66
pvaleso <amigotli@hotmail.com>
2012-07-16 23:47:00 -0500

    Segundom Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Premiosonda::Application.config.secret_token = '2b4b37cc11f021378d896bac8e9babd669e3bb625ac72d1c61a75708ac0eab8a328aa7511afcc02297ac920c8dc224821aafac23655a9104259b06fc8911ee3a'
